99678dbe45a5f7b11a7fbb72e6b2018eb1a37afe
Noor <luthfi.n57@gmail.com>
2025-02-25 20:01:43 +0700

    [FIX] Slider and popular destination

--- src/presentation/component/BannerSliderItem.tsx
+++ src/presentation/component/BannerSliderItem.tsx
@@ -18,10 +18,10 @@ const BannerSilderItem: React.FC<IBannerProps> = ({ banner }) => {
     return (
         <div
             key={banner.order}
-            className="top-0 left-0 w-full h-full flex ">
-            <img src={banner.image} className="w-full h-full object-cover "></img>
-            <div className="w-full h-full flex bg-black bg-opacity-30 text-white absolute items-center justify-center">
-                <div className={`${PAGE_WIDTH_CONFIG} w-full h-full flex flex-col justify-center items-start`}>
+            className="top-0 left-0 w-full h-full flex">
+            <img src={banner.image} className="w-full h-full object-cover"></img>
+            <div className="w-full h-full flex bg-black bg-opacity-30 text-white absolute">
+                <div className={`${PAGE_WIDTH_CONFIG} w-full h-full flex flex-col mt-44 sm:justify-center items-start`}>
                     <h2 className="text-[30px] font-bold mb-4 w-full font-alfaslab tracking-wider sm:mx-24 sm:text-[40px] lg:mx-32 lg:text-[57px] whitespace-pre-line text-center md:text-start">
                         {banner.bannerTitle}
                     </h2>
@@ -33,7 +33,7 @@ const BannerSilderItem: React.FC<IBannerProps> = ({ banner }) => {
                         className="px-4 pb-3 pt-2 bg-secondary hover:bg-orange-600 rounded-lg text-white font-alatsi mt-8 flex items-center text-xl sm:mx-24 lg:mx-32 self-center md:self-start"
                     >
                         {banner.btnText}
-                        <img src={ic_arrow_right} className="h-4 w-4 ml-3 mt-1"></img>
+                        <img src={ic_arrow_right} className="h-4 w-4 ml-3 mt-1 bottom-0"></img>
                     </a>
                 </div>
             </div>
--- src/presentation/pages/home/sections/PopularDestination.tsx
+++ src/presentation/pages/home/sections/PopularDestination.tsx
@@ -6,7 +6,7 @@ const PopularDestination = () => {
     return (
         <div className= "flex flex-col items-center justify-center">
             <p className="font-alice text-2xl text-center justify-self-center mt-[40px] sm:text-5xl">{getWcmsValue(SPOTLIGHT_DESTINATION_TITLE)}</p>
-            <p className="font-alike text-md justify-self-center mt-[20px] text-justify px-6 sm:px-[10%] sm:text-2xl md:px-[20%] lg:px-[30%]">
+            <p className="font-alike text-sm justify-self-center mt-[20px] text-justify px-6 sm:px-[10%] sm:text-2xl md:px-[20%] lg:px-[30%]">
                 {getWcmsValue(SPOTLIGHT_DESTINATION_DESC)}
             </p>
         </div>
